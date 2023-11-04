0b1131737d8997f8277e23cd9c32c9c78fb2193f
Igor <128895418+Dj-ikss@users.noreply.github.com>
2023-11-04 18:21:49 +0500

    add test BE - 2310
    add test BE - 2310

--- tests/test_parts.py
+++ tests/test_parts.py
@@ -9,6 +9,7 @@ from pages.locators import btn_menu_about, click_btn_menu_about, name_part, btn_
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import GO_MAIN_PAGE, SUMMER_ACTIVITY_BTN
 
 # BE - 2300 Раздел "Активности" - кнопки "Лето", "Зима", "Все сезоны"
 def test_part_activity_btn_winter_summer_allseason(driver):
@@ -34,5 +35,35 @@ def test_part_activity_btn_winter_summer_allseason(driver):
     driver.implicitly_wait(1)
     all_blocks_all_seasons = driver.find_elements(By.CSS_SELECTOR, '*[class="card_activity"]')
     assert len(all_blocks_all_seasons) == 8
-    driver.find_element(By.CSS_SELECTOR, '*[href="https://ecobelogorie.ru"]').click()              # Переходим на главную страницу по лого
+    GO_MAIN_PAGE(driver)                                                                          # Переходим на главную страницу по лого
 
+# BE - 2310 Летние виды развлечений в разделе "Активности"
+def test_part_activity_summer(driver):
+    btn_activity = driver.find_element(By.LINK_TEXT, 'Развлечения').get_attribute('href')    # Находим кнопку Развлечения
+    driver.get(btn_activity)                                                                 # Переходим по нопке в раздел "Активности"
+    SUMMER_ACTIVITY_BTN(driver)                                                              # Нажимаем на кнопку "Лето"
+    driver.implicitly_wait(1)
+    driver.find_elements(By.CSS_SELECTOR, '*[class="card_activity"]')[0].click()             # Нажимаем на первое изображение "Катание на квадроцикле"
+    part_name_atv = driver.find_element(*name_part.NAME_PART_SLIDER).text                    # Находим в открывшемся окне и сравнием название раздела
+    assert part_name_atv == 'Катание на квадроцикле'
+    driver.back()
+    SUMMER_ACTIVITY_BTN(driver)                                                              # Нажимаем на кнопку "Лето"
+    driver.find_elements(By.CSS_SELECTOR, '*[class="card_activity"]')[1].click()             # Нажимаем на второе изображение "Бадминтон"
+    part_name_badminton = driver.find_element(*name_part.NAME_PART_SLIDER).text              # Находим в открывшемся окне и сравнием название раздела
+    assert part_name_badminton == 'Бадминтон'
+    driver.back()
+    SUMMER_ACTIVITY_BTN(driver)                                                              # Нажимаем на кнопку "Лето"
+    driver.find_elements(By.CSS_SELECTOR, '*[class="card_activity"]')[2].click()             # Нажимаем на третье изображение "Катание на катамаране"
+    part_name_catamaran = driver.find_element(*name_part.NAME_PART_SLIDER).text              # Находим в открывшемся окне и сравнием название раздела
+    assert part_name_catamaran == 'Катание на катамаране'
+    driver.back()
+    SUMMER_ACTIVITY_BTN(driver)                                                              # Нажимаем на кнопку "Лето"
+    driver.find_elements(By.CSS_SELECTOR, '*[class="card_activity"]')[3].click()             # Нажимаем на изображение "Катание на SUP-Доске"
+    part_name_supboard = driver.find_element(*name_part.NAME_PART_SLIDER).text               # Находим в открывшемся окне и сравнием название раздела
+    assert part_name_supboard == 'Катание на SUP-Доске'
+    driver.back()
+    SUMMER_ACTIVITY_BTN(driver)                                                              # Нажимаем на кнопку "Лето"
+    driver.find_elements(By.CSS_SELECTOR, '*[class="card_activity"]')[4].click()             # Нажимаем на изображение "Стрельба из лука"
+    part_name_archery = driver.find_element(*name_part.NAME_PART_SLIDER).text                # Находим в открывшемся окне и сравнием название раздела
+    assert part_name_archery == 'Стрельба из лука'
+    GO_MAIN_PAGE(driver)
